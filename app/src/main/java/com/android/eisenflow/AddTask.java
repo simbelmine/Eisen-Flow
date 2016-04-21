04ae8b35455648e832cdd634400aa0873da0ad18
Svetoslava Delcheva <Sve@Svetoslavas-MacBook-Pro.local>
2016-04-21 16:16:02 -0700

    Add Task Activity : Header created

--- app/src/main/java/com/android/eisenflow/AddTask.java
+++ app/src/main/java/com/android/eisenflow/AddTask.java
@@ -1,18 +1,23 @@
 package com.android.eisenflow;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
 
 /**
  * Created by Sve on 4/21/16.
  */
-public class AddTask extends AppCompatActivity {
+public class AddTask extends Activity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.nav_header_main);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.add_task_lyout);
     }
 
     @Override
