7843d02b0bbce994f18554a57570b4d18220f799
Svetoslava Delcheva <Sve@Svetoslavas-MacBook-Pro.local>
2016-04-22 14:45:53 -0700

    Add Task Activity : Priority Dialog : declared radio buttons + clear rest if one button is selected

--- app/src/main/java/com/android/eisenflow/AddTask.java
+++ app/src/main/java/com/android/eisenflow/AddTask.java
@@ -1,13 +1,17 @@
 package com.android.eisenflow;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 /**
@@ -17,6 +21,10 @@ public class AddTask extends AppCompatActivity implements View.OnClickListener {
     private ImageView closeBtn;
     private TextView saveBtn;
     private LinearLayout priorityLayout;
+    RadioButton doIt;
+    RadioButton decide;
+    RadioButton delegate;
+    RadioButton dump;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -51,18 +59,76 @@ public class AddTask extends AppCompatActivity implements View.OnClickListener {
                 break;
             case R.id.task_add_save_btn:
                 // Save data
-                    // # To Do:
+                // # To Do:
 
                 finish();
                 overridePendingTransition(R.anim.slide_in_back, R.anim.slide_out_back);
                 break;
             case R.id.priority_layout:
-                AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.MyAlertDialogStyle);
-                builder.setTitle("Priority");
-                builder.setView(R.layout.priorities_dialog);
-                builder.setPositiveButton("OK", null);
-                builder.show();
+                final View dialogView = getInflatedPriorityDialog();
+                initPriorityDialogLayout(dialogView);
+                showPriorityDialog(dialogView);
                 break;
         }
     }
+
+    private View getInflatedPriorityDialog() {
+        LayoutInflater inflater = this.getLayoutInflater();
+        return inflater.inflate(R.layout.priorities_dialog, null);
+    }
+
+    private void initPriorityDialogLayout(View dialogView) {
+        doIt = (RadioButton) dialogView.findViewById(R.id.do_it);
+        decide = (RadioButton) dialogView.findViewById(R.id.decide_it);
+        delegate = (RadioButton) dialogView.findViewById(R.id.delegate_it);
+        dump = (RadioButton) dialogView.findViewById(R.id.dump_it);
+        doIt.setOnClickListener(radioBtnListener);
+        decide.setOnClickListener(radioBtnListener);
+        delegate.setOnClickListener(radioBtnListener);
+        dump.setOnClickListener(radioBtnListener);
+    }
+
+    private void showPriorityDialog(View dialogView) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.MyAlertDialogStyle);
+        builder.setTitle("Priority");
+        builder.setView(dialogView);
+        builder.setPositiveButton("OK", listener);
+        builder.show();
+    }
+
+    DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialogInterface, int i) {
+            // Save data
+                // # To Do:
+        }
+    };
+
+    View.OnClickListener radioBtnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()) {
+                case R.id.do_it:
+                    decide.setChecked(false);
+                    delegate.setChecked(false);
+                    dump.setChecked(false);
+                    break;
+                case R.id.decide_it:
+                    doIt.setChecked(false);
+                    delegate.setChecked(false);
+                    dump.setChecked(false);
+                    break;
+                case R.id.delegate_it:
+                    decide.setChecked(false);
+                    doIt.setChecked(false);
+                    dump.setChecked(false);
+                    break;
+                case R.id.dump_it:
+                    decide.setChecked(false);
+                    delegate.setChecked(false);
+                    doIt.setChecked(false);
+                    break;
+            }
+        }
+    };
 }
