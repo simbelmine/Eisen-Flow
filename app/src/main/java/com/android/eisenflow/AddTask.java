5076a7d86b29295af4a956270b1effcb9a9c3f77
Svetoslava Delcheva <Sve@Svetoslavas-MacBook-Pro.local>
2016-04-21 18:02:16 -0700

    Add Task Activity : add simple button functionality

--- app/src/main/java/com/android/eisenflow/AddTask.java
+++ app/src/main/java/com/android/eisenflow/AddTask.java
@@ -4,13 +4,18 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by Sve on 4/21/16.
  */
-public class AddTask extends Activity {
+public class AddTask extends AppCompatActivity implements View.OnClickListener {
+    private ImageView closeBtn;
+    private TextView saveBtn;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -18,11 +23,37 @@ public class AddTask extends Activity {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         getWindow().requestFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.add_task_lyout);
+
+        initLayout();
+    }
+
+    private void initLayout() {
+        closeBtn = (ImageView) findViewById(R.id.task_add_close_btn);
+        closeBtn.setOnClickListener(this);
+        saveBtn = (TextView) findViewById(R.id.task_add_save_btn);
+        saveBtn.setOnClickListener(this);
     }
 
     @Override
     public void onBackPressed() {
         super.onBackPressed();
         overridePendingTransition(R.anim.slide_in_back, R.anim.slide_out_back);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.task_add_close_btn:
+                finish();
+                overridePendingTransition(R.anim.slide_in_back, R.anim.slide_out_back);
+                break;
+            case R.id.task_add_save_btn:
+                // Save data
+                // # To Do:
+
+                finish();
+                overridePendingTransition(R.anim.slide_in_back, R.anim.slide_out_back);
+                break;
+        }
     }
 }
