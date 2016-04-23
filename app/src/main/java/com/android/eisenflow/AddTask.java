a759f5614ec5f345505c6e7fb08cef75b12e886a
Svetoslava Delcheva <Sve@Svetoslavas-MacBook-Pro.local>
2016-04-22 23:00:25 -0700

    Add Task Activity : New Priority Picker + Animation when click to change Background

--- app/src/main/java/com/android/eisenflow/AddTask.java
+++ app/src/main/java/com/android/eisenflow/AddTask.java
@@ -1,17 +1,16 @@
 package com.android.eisenflow;
 
-import android.content.DialogInterface;
+import android.animation.ValueAnimator;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.RadioButton;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -22,11 +21,12 @@ public class AddTask extends AppCompatActivity implements View.OnClickListener {
     private ImageView closeBtn;
     private TextView saveBtn;
     private LinearLayout priorityLayout;
-    private RadioButton doIt;
-    private RadioButton decide;
-    private RadioButton delegate;
-    private RadioButton dump;
     private RelativeLayout addTaskBg;
+
+    private LinearLayout doItLayout;
+    private LinearLayout decideItLayout;
+    private LinearLayout delegateItLayout;
+    private LinearLayout dumpItLayout;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -45,6 +45,16 @@ public class AddTask extends AppCompatActivity implements View.OnClickListener {
         priorityLayout = (LinearLayout) findViewById(R.id.priority_layout);
         priorityLayout.setOnClickListener(this);
         addTaskBg = (RelativeLayout) findViewById(R.id.add_task_bg);
+
+        doItLayout = (LinearLayout) findViewById(R.id.do_it_l);
+        decideItLayout = (LinearLayout) findViewById(R.id.decide_it_l);
+        delegateItLayout = (LinearLayout) findViewById(R.id.delegate_it_l);
+        dumpItLayout = (LinearLayout) findViewById(R.id.dump_it_l);
+
+        doItLayout.setOnClickListener(this);
+        decideItLayout.setOnClickListener(this);
+        delegateItLayout.setOnClickListener(this);
+        dumpItLayout.setOnClickListener(this);
     }
 
     @Override
@@ -68,98 +78,54 @@ public class AddTask extends AppCompatActivity implements View.OnClickListener {
                 overridePendingTransition(R.anim.slide_in_back, R.anim.slide_out_back);
                 break;
             case R.id.priority_layout:
-                final View dialogView = getInflatedPriorityDialog();
-                initPriorityDialogLayout(dialogView);
-                showPriorityDialog(dialogView);
+                break;
+            case R.id.do_it_l:
+                setBackgroundWithAnimation(R.color.firstQuadrant);
+                break;
+            case R.id.decide_it_l:
+                setBackgroundWithAnimation(R.color.secondQuadrant);
+                break;
+            case R.id.delegate_it_l:
+                setBackgroundWithAnimation(R.color.thirdQuadrant);
+                break;
+            case R.id.dump_it_l:
+                setBackgroundWithAnimation(R.color.fourthQuadrant);
                 break;
         }
     }
 
-    private View getInflatedPriorityDialog() {
-        LayoutInflater inflater = this.getLayoutInflater();
-        return inflater.inflate(R.layout.priorities_dialog, null);
-    }
+    private void setBackgroundWithAnimation(final int toColor) {
+        final int color = getBackgroundColor();
 
-    private void initPriorityDialogLayout(View dialogView) {
-        doIt = (RadioButton) dialogView.findViewById(R.id.do_it);
-        decide = (RadioButton) dialogView.findViewById(R.id.decide_it);
-        delegate = (RadioButton) dialogView.findViewById(R.id.delegate_it);
-        dump = (RadioButton) dialogView.findViewById(R.id.dump_it);
-        doIt.setOnClickListener(radioBtnListener);
-        decide.setOnClickListener(radioBtnListener);
-        delegate.setOnClickListener(radioBtnListener);
-        dump.setOnClickListener(radioBtnListener);
-    }
+        ValueAnimator anim = ValueAnimator.ofFloat(0, 1);
+        anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float position = animation.getAnimatedFraction();
+                int blended = blendColors(color, getResources().getColor(toColor), position);
 
-    private void showPriorityDialog(View dialogView) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.MyAlertDialogStyle);
-        builder.setTitle("Priority");
-        builder.setView(dialogView);
-        builder.setPositiveButton("OK", listenerOkBtn);
-        builder.show();
-    }
-
-    DialogInterface.OnClickListener listenerOkBtn = new DialogInterface.OnClickListener() {
-        @Override
-        public void onClick(DialogInterface dialogInterface, int i) {
-            RadioButton checkedBtnId = getCheckedBtnId();
-            if(checkedBtnId != null) {
-                setTaskColor(checkedBtnId);
+                // Apply blended color to the view.
+                addTaskBg.setBackgroundColor(blended);
             }
-        }
-    };
-
-    private void setTaskColor(RadioButton checkedBtnId) {
-        switch (checkedBtnId.getId()) {
-            case R.id.do_it:
-                addTaskBg.setBackgroundColor(getResources().getColor(R.color.firstQuadrant));
-                break;
-            case R.id.decide_it:
-                addTaskBg.setBackgroundColor(getResources().getColor(R.color.secondQuadrant));
-                break;
-            case R.id.delegate_it:
-                addTaskBg.setBackgroundColor(getResources().getColor(R.color.thirdQuadrant));
-                break;
-            case R.id.dump_it:
-                addTaskBg.setBackgroundColor(getResources().getColor(R.color.fourthQuadrant));
-                break;
-        }
+        });
+        anim.start();
     }
 
-    private RadioButton getCheckedBtnId() {
-        if(doIt.isChecked()) return doIt;
-        if(decide.isChecked()) return decide;
-        if(delegate.isChecked()) return delegate;
-        if(dump.isChecked()) return dump;
-
-        return null;
+    private int getBackgroundColor() {
+        Drawable bg = addTaskBg.getBackground();
+        if(bg instanceof ColorDrawable)
+            return ((ColorDrawable) bg).getColor();
+        else
+            return Color.TRANSPARENT;
     }
 
-    View.OnClickListener radioBtnListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            switch (view.getId()) {
-                case R.id.do_it:
-                    decide.setChecked(false);
-                    delegate.setChecked(false);
-                    dump.setChecked(false);
-                    break;
-                case R.id.decide_it:
-                    doIt.setChecked(false);
-                    delegate.setChecked(false);
-                    dump.setChecked(false);
-                    break;
-                case R.id.delegate_it:
-                    decide.setChecked(false);
-                    doIt.setChecked(false);
-                    dump.setChecked(false);
-                    break;
-                case R.id.dump_it:
-                    decide.setChecked(false);
-                    delegate.setChecked(false);
-                    doIt.setChecked(false);
-                    break;
-            }
-        }
-    };
+    private int blendColors(int from, int to, float ratio) {
+        final float inverseRatio = 1f - ratio;
+
+        final float r = Color.red(to) * ratio + Color.red(from) * inverseRatio;
+        final float g = Color.green(to) * ratio + Color.green(from) * inverseRatio;
+        final float b = Color.blue(to) * ratio + Color.blue(from) * inverseRatio;
+
+        return Color.rgb((int) r, (int) g, (int) b);
+    }
 }
