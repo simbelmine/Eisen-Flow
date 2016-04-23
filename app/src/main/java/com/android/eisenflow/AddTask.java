637dd834396c5f5237af00574e78f35cf82483c6
Svetoslava Delcheva <Sve@Svetoslavas-MacBook-Pro.local>
2016-04-23 12:42:53 -0700

    Add New Task : Date&Time : show date and time + expand/collapse Calendar/Time Picker

--- app/src/main/java/com/android/eisenflow/AddTask.java
+++ app/src/main/java/com/android/eisenflow/AddTask.java
@@ -9,10 +9,16 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 /**
  * Created by Sve on 4/21/16.
@@ -27,6 +33,11 @@ public class AddTask extends AppCompatActivity implements View.OnClickListener {
     private LinearLayout decideItLayout;
     private LinearLayout delegateItLayout;
     private LinearLayout dumpItLayout;
+
+    private LinearLayout calendarView;
+    private TimePicker timePickerView;
+    private TextView dateTxt;
+    private TextView timeTxt;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -55,6 +66,16 @@ public class AddTask extends AppCompatActivity implements View.OnClickListener {
         decideItLayout.setOnClickListener(this);
         delegateItLayout.setOnClickListener(this);
         dumpItLayout.setOnClickListener(this);
+
+        calendarView = (LinearLayout) findViewById(R.id.add_task_calendar);
+        timePickerView = (TimePicker) findViewById(R.id.add_task_time_picker);
+        timePickerView.setVisibility(View.GONE);
+        dateTxt = (TextView) findViewById(R.id.add_task_date_txt);
+        dateTxt.setText(getCurrentDateString());
+        dateTxt.setOnClickListener(this);
+        timeTxt = (TextView) findViewById(R.id.add_task_time);
+        timeTxt.setText(getCurrentTimeString());
+        timeTxt.setOnClickListener(this);
     }
 
     @Override
@@ -91,7 +112,78 @@ public class AddTask extends AppCompatActivity implements View.OnClickListener {
             case R.id.dump_it_l:
                 setBackgroundWithAnimation(R.color.fourthQuadrant);
                 break;
+            case R.id.add_task_date_txt:
+                if(calendarView.getVisibility() == View.VISIBLE) {
+                    collapse(calendarView);
+                }
+                else {
+                    collapse(timePickerView);
+                    expand(calendarView);
+                }
+                break;
+            case R.id.add_task_time:
+                if(timePickerView.getVisibility() == View.VISIBLE) {
+                    collapse(timePickerView);
+                }
+                else {
+                    collapse(calendarView);
+                    expand(timePickerView);
+                }
         }
+    }
+
+    public static void expand(final View v) {
+        v.measure(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        final int targetHeight = v.getMeasuredHeight();
+
+        // Older versions of android (pre API 21) cancel animations for views with a height of 0.
+        v.getLayoutParams().height = 1;
+        v.setVisibility(View.VISIBLE);
+        Animation a = new Animation()
+        {
+            @Override
+            protected void applyTransformation(float interpolatedTime, Transformation t) {
+                v.getLayoutParams().height = interpolatedTime == 1
+                        ? LinearLayout.LayoutParams.WRAP_CONTENT
+                        : (int)(targetHeight * interpolatedTime);
+                v.requestLayout();
+            }
+
+            @Override
+            public boolean willChangeBounds() {
+                return true;
+            }
+        };
+
+        // 1dp/ms
+        a.setDuration((int)(targetHeight / v.getContext().getResources().getDisplayMetrics().density));
+        v.startAnimation(a);
+    }
+
+    public static void collapse(final View v) {
+        final int initialHeight = v.getMeasuredHeight();
+
+        Animation a = new Animation()
+        {
+            @Override
+            protected void applyTransformation(float interpolatedTime, Transformation t) {
+                if(interpolatedTime == 1){
+                    v.setVisibility(View.GONE);
+                }else{
+                    v.getLayoutParams().height = initialHeight - (int)(initialHeight * interpolatedTime);
+                    v.requestLayout();
+                }
+            }
+
+            @Override
+            public boolean willChangeBounds() {
+                return true;
+            }
+        };
+
+        // 1dp/ms
+        a.setDuration((int)(initialHeight / v.getContext().getResources().getDisplayMetrics().density));
+        v.startAnimation(a);
     }
 
     private void setBackgroundWithAnimation(final int toColor) {
@@ -128,4 +220,15 @@ public class AddTask extends AppCompatActivity implements View.OnClickListener {
 
         return Color.rgb((int) r, (int) g, (int) b);
     }
+
+    private String getCurrentDateString() {
+        SimpleDateFormat postFormater = new SimpleDateFormat("EEE, MMM dd, yyyy");
+        return postFormater.format(Calendar.getInstance().getTime());
+    }
+
+    private String getCurrentTimeString() {
+        SimpleDateFormat postFormater = new SimpleDateFormat("kk:mm");
+        return postFormater.format(Calendar.getInstance().getTime());
+    }
 }
+
