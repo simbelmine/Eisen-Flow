3fd6b19442ae2a0a0fcb7efe107df9f9d843e5c5
Svetoslava Delcheva <Sve@Svetoslavas-MacBook-Pro.local>
2016-07-20 09:50:35 -0700

    Fixed About Dialog

--- app/src/main/java/com/android/eisenflow/AboutDIalog.java
+++ app/src/main/java/com/android/eisenflow/AboutDIalog.java
@@ -5,7 +5,6 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
